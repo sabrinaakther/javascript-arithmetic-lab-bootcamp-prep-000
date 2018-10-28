225ee6f1843784bd70e0534e36f10c691985411f
Sabrina Akther <sabrinasobhan.92@gmail.com>
2018-10-28 05:03:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var x= addition(5,7)
+function add(a,b) {
+  return (a + b);
+}
+var x = subtract(30,52)
+function subtract(a,b) {
+  return (a - b);
+}
+var x= multiply(3,7)
+function multiply(a,b) {
+  return (a * b);
+}
+var x =divide(2,6)
+function divide(a,b) {
+  return (b / a);
+}
